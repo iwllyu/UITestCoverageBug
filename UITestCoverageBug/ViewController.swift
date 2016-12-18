8784d07c11fd1b7994a4ee52e414e1bc9a43e779
Andrei Raifura <andrei.raifura@yopeso.com>
2016-12-18 12:26:41 +0200

    Migrate the codebase to Swift 3.0
    The project is compatible with Xcode 8.2

--- UITestCoverageBug/ViewController.swift
+++ UITestCoverageBug/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         print("Reached via unit test")
     }
     
-    @IBAction func testAction(sender: AnyObject) {
+    @IBAction func testAction(_ sender: AnyObject) {
         testedViaUITest()
     }
     
